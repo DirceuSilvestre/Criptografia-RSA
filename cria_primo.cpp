1ca4b7c70cd13258ffb41b79b20ca767ada8dd52
Dirceu Silvestre <dirceu_silvestre@hotmail.com>
2022-05-22 11:21:33 -0300

    first commit - gerador de primos criado e funcionando

---
+++ cria_primo.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+/*
+
+    Código criado para gerar 2 números primos e diferentes entre si
+
+    os primos são importantes pois a partir deles conseguiremos criar a chave pública e a chave privada
+
+*/
+
+using namespace std;
+
+// typedef unsigned short int usi;
+
+int primo1, primo2, anterior = 0;
+
+// Protótipos
+
+void cria_primo(int *onde_guardar);
+
+void guarda_primo(int primo);
+
+int main()
+{
+    cria_primo(&primo1);
+    cria_primo(&primo2);
+
+    guarda_primo(primo1);
+    guarda_primo(primo2);
+
+    return 0;
+}
+
+void guarda_primo(int primo)
+{
+    char texto[sizeof(int)];
+    ofstream fout("Chave_RSA.txt", ios::app);
+    sprintf(texto, "%d", primo);
+    fout << texto << endl;
+}
+
+void cria_primo(int *onde_guardar)
+{
+    int gerador_de_numero;
+    srand(time(NULL));
+    gerador_de_numero = rand();
+    while (gerador_de_numero < 10000)
+    {
+        gerador_de_numero = rand();
+    }
+
+    while (1)
+    {
+        int i;
+        int divisores = 0;
+
+        for (i = 2; i < gerador_de_numero / 2; i++)
+        {
+            if (gerador_de_numero % i == 0)
+            {
+                divisores++;
+            }
+            else if (divisores != 0)
+            {
+                break;
+            }
+        }
+        if (divisores == 0 && gerador_de_numero != anterior)
+        {
+            *onde_guardar = gerador_de_numero;
+            anterior = gerador_de_numero;
+            break;
+        }
+        else
+        {
+            gerador_de_numero = rand();
+        }
+    }
+}
